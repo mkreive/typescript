40fd014a6f22c72ee2b28941740f02f765bed65e
mkreive <mkreive@gmail.com>
2022-03-04 11:02:40 +0200

    generic

--- basics.ts
+++ basics.ts
@@ -23,3 +23,41 @@ person = {
     age: 36,
     city: "Kaunas",
 };
+
+//Union type - multiple types
+let course: string | number;
+course = 12123;
+course = "react course";
+
+//Type allies
+type Animal = {
+    name: string;
+    age: number;
+};
+
+let dog: Animal;
+let cat: Animal;
+
+dog = {
+    name: "Moka",
+    age: 1,
+};
+
+//Function and types
+function add(a: number, b: number) {
+    return a + b;
+}
+
+function print(value: any) {
+    console.log(value); //void like null
+}
+
+// Generics
+function insertAtBeginning<T>(array: T[], value: T) {
+    const newArray = [value, ...array];
+    return newArray;
+}
+
+const demoArray = [1, 2, 3, 4];
+const updatedArray = insertAtBeginning(demoArray, -1);
+const stringArray = insertAtBeginning(["a", "b", "c"], "z");
