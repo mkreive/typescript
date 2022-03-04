eb4bd1fca6cc3f61f734b401de6adb399a254993
mkreive <mkreive@gmail.com>
2022-03-04 10:36:10 +0200

    object arrays

---
+++ basics.ts
@@ -0,0 +1,25 @@
+//Primitives
+let age: number;
+age = 12;
+
+let userName: string;
+userName = "Moniika";
+
+let isWar: boolean;
+isWar = true;
+
+//Objects, Arrays, etc
+let hobbies: string[]; //numbers[]
+hobbies = ["Board games", "Travel", "Books"];
+
+// let person: any;
+let person: {
+    name: string;
+    age: number;
+    city: string;
+};
+person = {
+    name: "Monika",
+    age: 36,
+    city: "Kaunas",
+};
